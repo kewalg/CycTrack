1362cf40d537de89efc832fadc9eb8c3294cad37
kewalg <kewalgulve007@gmail.com>
2020-04-14 12:34:11 -0400

    basic setup of maps with self location

--- app/src/main/java/com/example/cyctrack/MainActivity.java
+++ app/src/main/java/com/example/cyctrack/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.cyctrack;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng Home = new LatLng(42.102767, -75.933271);
+        mMap.addMarker(new MarkerOptions().position(Home).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(Home));
     }
 }
