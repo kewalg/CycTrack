bad407326803942d55ff0f8f53108b17669ad9d5
Rohan Chutke <rohanchutke@Rohans-MacBook-Pro.local>
2020-04-16 17:21:10 +1000

    review incorporated in main app

--- app/src/main/java/com/example/cyctrack/FeedBackActivity.java
+++ app/src/main/java/com/example/cyctrack/FeedBackActivity.java
@@ -1,25 +1,88 @@
 package com.example.cyctrack;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class FeedBackActivity extends AppCompatActivity {
+    UserReview myDb;
 
-    Button btn_submit;
-            Button btn_feedback;
-            EditText edt_name;
-            EditText edt_feedback;
+    Button btnSubmit;
+    Button btnFeedback;
+    EditText editName;
+    EditText editFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed_back);
 
-        btn_submit = findViewById(R.id.btn_submit);
-        
+        myDb = new UserReview(this);
+
+        btnSubmit = findViewById(R.id.btnSubmit);
+        btnFeedback = findViewById(R.id.btnFeedback);
+        editName = findViewById(R.id.editName);
+        editFeedback =findViewById(R.id.editFeedback);
+
+        SubmitReview();
+        viewFeedbacks();
+
+
+
+
+    }
+    public void SubmitReview(){
+        btnSubmit.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isInserted = myDb.insertData(editName.getText().toString(),
+                                editFeedback.getText().toString());
+                        if(isInserted == true)
+                            Toast.makeText(FeedBackActivity.this, "Review Submitted Successfully!! Thanks =)",Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(FeedBackActivity.this,"Please submit your feedback :(",Toast.LENGTH_LONG).show();
+
+                    }
+                }
+        );
+    }
+
+    public void viewFeedbacks(){
+        btnFeedback.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Cursor res = myDb.readFeedbacks();
+                        if(res.getCount() == 0){
+                            //show message
+                            showMessage("Error","No Feedbacks Found");
+                            return;
+                        }
+                        StringBuffer buffer = new StringBuffer();
+                        while(res.moveToNext()){
+                            buffer.append("Name: "+res.getString(1)+"\n");
+                            buffer.append("Feedback: "+res.getString(2)+"\n\n");
+                        }
+                        // show all data
+                        showMessage("Feedbacks Found !",buffer.toString());
+                    }
+                }
+        );
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
 
     }
 }
