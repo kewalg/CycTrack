d81a88fe754778c92cfeaeaf7e48e7bb36f58a4b
kewalg <kewalgulve007@gmail.com>
2020-04-21 15:45:00 -0400

    fixed the database list issue and basic UI

--- app/src/main/java/com/example/cyctrack/MainActivity.java
+++ app/src/main/java/com/example/cyctrack/MainActivity.java
@@ -9,11 +9,17 @@ import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.media.Image;
 import android.media.MicrophoneDirection;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toolbar;
+
+import com.example.cyctrack.Model.Main;
+import com.example.cyctrack.Model.Weather;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,17 +29,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //toaaccess notification service
+        ImageView img_maps, img_weather, img_reviews, img_speedo;
+
+        img_maps = findViewById(R.id.img_maps);
+        img_weather = findViewById(R.id.img_weather);
+        img_reviews = findViewById(R.id.img_reviews);
+        img_speedo = findViewById(R.id.img_speedo);
+
+
+        img_maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MapsActivity.class);
+                startActivity(i);
+            }
+        });
+
+        img_weather.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, WeatherActivity.class);
+                startActivity(i);
+            }
+        });
+
+        img_reviews.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ReviewActivity.class);
+                startActivity(i);
+            }
+        });
+
+        img_speedo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SpeedOMeter.class);
+                startActivity(i);
+            }
+        });
+
+     /*   //toaaccess notification service
         NotificationManager n = (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
         //permissions granted?
         if (n.isNotificationPolicyAccessGranted()) {
             n.setInterruptionFilter(NotificationManager.INTERRUPTION_FILTER_NONE);
-            /*Intent i = new Intent(MainActivity.this, WeatherActivity.class);
-            startActivity(i);*/
+            *//*Intent i = new Intent(MainActivity.this, WeatherActivity.class);
+            startActivity(i);*//*
         } else {
             // Ask the user to grant access
             Intent intent = new Intent(android.provider.Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
             startActivity(intent);
-        }
+        }*/
     }
 }
