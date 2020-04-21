7124bc7f5c6c8920e8625caf3acb9845f23d34c4
Rohan Chutke <rohanchutke@Rohans-MacBook-Pro.local>
2020-04-21 20:20:22 +1000

    Changes done to review

--- app/src/main/java/com/example/cyctrack/ReviewActivity.java
+++ app/src/main/java/com/example/cyctrack/ReviewActivity.java
@@ -1,7 +1,6 @@
 package com.example.cyctrack;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,13 +10,16 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ReviewActivity extends AppCompatActivity implements View.OnClickListener {
 
     Button buttonAddItem;
+    Button buttonListItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
 
-        buttonAddItem = (Button)findViewById(R.id.btn_add_item);
+        buttonAddItem = findViewById(R.id.btn_add_item);
+        buttonListItem = findViewById(R.id.btn_list_items);
         buttonAddItem.setOnClickListener(this);
+        buttonListItem.setOnClickListener(this);
 
     }
 
@@ -27,6 +29,12 @@ public class ReviewActivity extends AppCompatActivity implements View.OnClickLis
         if(v==buttonAddItem){
 
             Intent intent = new Intent(getApplicationContext(),AddItem.class);
+            startActivity(intent);
+        }
+
+        if(v==buttonListItem){
+
+            Intent intent = new Intent(getApplicationContext(), ListItem.class);
             startActivity(intent);
         }
 
